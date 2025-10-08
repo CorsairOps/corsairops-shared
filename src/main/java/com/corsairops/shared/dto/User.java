87064ca11b1c7f78b3532a5eda9590af3e8ac7df
Tyler Wade <vaxonreal@gmail.com>
2025-10-08 17:10:31 -0400

    Authentication overhaul. Authentication service migrated to keycloak. User Service refactor. Rbac changes. Mission service request fixes.

--- src/main/java/com/corsairops/shared/dto/User.java
+++ src/main/java/com/corsairops/shared/dto/User.java
@@ -2,9 +2,11 @@ package com.corsairops.shared.dto;
 
 public record User(
         String id,
+        String username,
         String email,
-        String givenName,
-        String familyName,
-        String gender
+        String firstName,
+        String lastName,
+        boolean enabled,
+        Long createdTimestamp
 ) {
 }
